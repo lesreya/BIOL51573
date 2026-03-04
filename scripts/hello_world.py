7700956f342f34778046c9bdaf9f50967e49af9d
Leslie Reyes <lreyes4@uark.edu>
2026-03-04 10:31:20 -0600

    first commit from class 04-Mar-2026

--- scripts/hello_world.py
+++ scripts/hello_world.py
@@ -1,10 +1,4 @@
-#!/usr/bin/env python3
+#! /usr/bin/env python3
 
-name = input("enter your name: ")
-
-#say 'hello world'
+print("hello world")
 print('hello world')
-
-#print greeting with the users name
-print('hello', name)
-
